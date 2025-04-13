edb815d294c303a29edce507da25a600db7f1b96
Nicki tst <kanieleoutiz@gmail.com>
2025-04-14 00:22:20 +0300

    add get search update delete

--- server.py
+++ server.py
@@ -18,3 +18,23 @@ class CreateAdvRequest(BaseModel):
 @app.post("/api/v1/advertisement/", tags=["advertisements"])
 async def create_advertisement(adv: CreateAdvRequest):
     return {"message": "Hello World"}
+
+
+@app.get("/api/v1/advertisement/{adv_id}", tags=["advertisements"])
+async def get_advertisement(adv: CreateAdvRequest):
+    return {"message": "Hello World"}
+
+
+@app.get("/api/v1/advertisement/{adv_id}?{query_string}", tags=["advertisements"])
+async def search_advertisement(adv: CreateAdvRequest):
+    return {"message": "Hello World"}
+
+
+@app.patch("/api/v1/advertisement/{adv_id}", tags=["advertisements"])
+async def update_advertisement(adv: CreateAdvRequest):
+    return {"message": "Hello World"}
+
+
+@app.delete("/api/v1/advertisement/{adv_id}", tags=["advertisements"])
+async def delete_advertisement(adv: CreateAdvRequest):
+    return {"message": "Hello World"}
