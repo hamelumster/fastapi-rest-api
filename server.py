53e719c2f75583f19bf47433b68a0b7fb8985fe2
Nicki tst <kanieleoutiz@gmail.com>
2025-04-14 00:11:49 +0300

    add CreateAdvRequest class

--- server.py
+++ server.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+from datetime import datetime
 
 app = FastAPI(
     title="Purchase and Sale Service",
     description="List of advertisements",
 )
 
-@app.post("/api/v1/advertisement/")
-async def create_advertisement():
+class CreateAdvRequest(BaseModel):
+    title: str
+    description: str
+    price: float
+    author: str
+    created_at: datetime
+
+@app.post("/api/v1/advertisement/", tags=["advertisements"])
+async def create_advertisement(adv: CreateAdvRequest):
     return {"message": "Hello World"}
