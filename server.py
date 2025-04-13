30d6e23bf32c48c37141cc3204cf52e7c336b786
Nicki tst <kanieleoutiz@gmail.com>
2025-04-14 00:43:20 +0300

    change parameters for create get search update delete

--- server.py
+++ server.py
@@ -1,19 +1,11 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-
-from datetime import datetime
+from schema import CreateAdvRequest, UpdateAdvRequest
 
 app = FastAPI(
     title="Purchase and Sale Service",
     description="List of advertisements",
 )
 
-class CreateAdvRequest(BaseModel):
-    title: str
-    description: str
-    price: float
-    author: str
-    created_at: datetime
 
 @app.post("/api/v1/advertisement/", tags=["advertisements"])
 async def create_advertisement(adv: CreateAdvRequest):
@@ -21,20 +13,21 @@ async def create_advertisement(adv: CreateAdvRequest):
 
 
 @app.get("/api/v1/advertisement/{adv_id}", tags=["advertisements"])
-async def get_advertisement(adv: CreateAdvRequest):
+async def get_advertisement(adv_id: int):
     return {"message": "Hello World"}
 
 
 @app.get("/api/v1/advertisement/{adv_id}?{query_string}", tags=["advertisements"])
-async def search_advertisement(adv: CreateAdvRequest):
+async def search_advertisement(title: str = None, description: str = None,
+                               price: float = None, author: str = None):
     return {"message": "Hello World"}
 
 
 @app.patch("/api/v1/advertisement/{adv_id}", tags=["advertisements"])
-async def update_advertisement(adv: CreateAdvRequest):
+async def update_advertisement(adv_id: int, adv_data: UpdateAdvRequest):
     return {"message": "Hello World"}
 
 
 @app.delete("/api/v1/advertisement/{adv_id}", tags=["advertisements"])
-async def delete_advertisement(adv: CreateAdvRequest):
+async def delete_advertisement(adv_id: int):
     return {"message": "Hello World"}
