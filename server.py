04f6b18321d045f4310cc8dfac76675d5b1f0093
Nicki tst <kanieleoutiz@gmail.com>
2025-04-14 09:02:44 +0300

    update: get search post delete update

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
-from schema import CreateAdvRequest, UpdateAdvRequest
+from schema import (CreateAdvRequest, UpdateAdvRequest, CreateAdvResponse,
+                    GetAdvResponse, SearchAdvResponse, UpdateAdvResponse,
+                    DeleteAdvResponse)
 
 app = FastAPI(
     title="Purchase and Sale Service",
@@ -7,27 +9,37 @@ app = FastAPI(
 )
 
 
-@app.post("/api/v1/advertisement/", tags=["advertisements"])
+@app.post("/api/v1/advertisement/",
+          tags=["advertisements"],
+          response_model=CreateAdvResponse)
 async def create_advertisement(adv: CreateAdvRequest):
     return {"message": "Hello World"}
 
 
-@app.get("/api/v1/advertisement/{adv_id}", tags=["advertisements"])
+@app.get("/api/v1/advertisement/{adv_id}",
+         tags=["advertisements"],
+         response_model=GetAdvResponse)
 async def get_advertisement(adv_id: int):
     return {"message": "Hello World"}
 
 
-@app.get("/api/v1/advertisement/{adv_id}?{query_string}", tags=["advertisements"])
+@app.get("/api/v1/advertisement/{adv_id}?{query_string}",
+         tags=["advertisements"],
+         response_model=SearchAdvResponse)
 async def search_advertisement(title: str = None, description: str = None,
                                price: float = None, author: str = None):
     return {"message": "Hello World"}
 
 
-@app.patch("/api/v1/advertisement/{adv_id}", tags=["advertisements"])
+@app.patch("/api/v1/advertisement/{adv_id}",
+           tags=["advertisements"],
+           response_model=UpdateAdvResponse)
 async def update_advertisement(adv_id: int, adv_data: UpdateAdvRequest):
     return {"message": "Hello World"}
 
 
-@app.delete("/api/v1/advertisement/{adv_id}", tags=["advertisements"])
+@app.delete("/api/v1/advertisement/{adv_id}",
+            tags=["advertisements"],
+            response_model=DeleteAdvResponse)
 async def delete_advertisement(adv_id: int):
     return {"message": "Hello World"}
