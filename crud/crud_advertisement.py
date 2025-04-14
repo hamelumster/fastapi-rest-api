86903535eceb1523db6eaf496c86e5158bd11e78
Nicki tst <kanieleoutiz@gmail.com>
2025-04-14 11:23:43 +0300

    add crud folder with separate crud for user and adv-t

---
+++ crud/crud_advertisement.py
@@ -0,0 +1,17 @@
+from fastapi import HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from db.models import ORM_OBJ_ADV_CLS
+
+
+async def add_advertisement(session: AsyncSession, adv: ORM_OBJ_ADV_CLS):
+    session.add(adv)
+    await session.commit()
+
+
+async def get_adv_by_id(session: AsyncSession, adv_orm_cls: ORM_OBJ_ADV_CLS, adv_id: int):
+    adv_orm_obj = await session.get(adv_orm_cls, adv_id)
+    if adv_orm_obj is None:
+        raise HTTPException(404, detail="Advertisement not found")
+    return adv_orm_obj
+
---
+++ crud/crud_user.py
@@ -0,0 +1,19 @@
+from fastapi import HTTPException
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from db.models import ORM_OBJ_USR_CLS
+
+
+async def add_user(session: AsyncSession, user: ORM_OBJ_USR_CLS):
+    session.add(user)
+    try:
+        await session.commit()
+    except IntegrityError:
+        HTTPException(409, detail="User already exists")
+
+async def get_user_by_id(session: AsyncSession, user_orm_cls: ORM_OBJ_USR_CLS, user_id: int):
+    user_orm_obj = await session.get(user_orm_cls, user_id)
+    if user_orm_obj is None:
+        raise HTTPException(404, detail="User not found")
+    return user_orm_obj
