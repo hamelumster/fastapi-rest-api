641c0d4664f6e449f77446131852945681a312e2
Nicki tst <kanieleoutiz@gmail.com>
2025-04-14 08:59:56 +0300

    add create get search success update delete Responses

--- schema.py
+++ schema.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 from datetime import datetime
+from typing import Literal
 
 
 class CreateAdvRequest(BaseModel):
@@ -14,3 +15,32 @@ class UpdateAdvRequest(BaseModel):
     title: str | None = None
     description: str | None = None
     price: float | None = None
+
+
+class CreateAdvResponse(BaseModel):
+    id: int
+
+
+class GetAdvResponse(BaseModel):
+    id: int
+    title: str
+    description: str
+    price: float
+    author: str
+    created_at: datetime
+
+
+class SearchAdvResponse(BaseModel):
+    results: list[GetAdvResponse]
+
+
+class SuccessAdvResponse(BaseModel):
+    status: Literal["success"]
+
+
+class UpdateAdvResponse(SuccessAdvResponse):
+    pass
+
+
+class DeleteAdvResponse(SuccessAdvResponse):
+    pass
