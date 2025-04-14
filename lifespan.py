9abb42fb984cedde2d89414b205f58cc5267abcb
Nicki tst <kanieleoutiz@gmail.com>
2025-04-14 09:59:24 +0300

    add lifespan.py

---
+++ lifespan.py
@@ -0,0 +1,12 @@
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+
+from models import init_orm, close_orm
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await init_orm()
+    yield
+    await close_orm()
