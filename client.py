5a744defbf480488322f8204c4dabdb8e3f3d720
Nicki tst <kanieleoutiz@gmail.com>
2025-04-15 23:57:29 +0300

    add get_user request

--- client.py
+++ client.py
@@ -1,10 +1,15 @@
 import requests
 
 # Создание пользователя
-create_user = requests.post(
-    "http://127.0.0.1:8000/api/v1/user/",
-    json={"username": "user_4", "password": "test_password"}
-)
-print(create_user.status_code)
-print(create_user.json())
+# create_user = requests.post(
+#     "http://127.0.0.1:8000/api/v1/user/",
+#     json={"username": "user_4", "password": "test_password"}
+# )
+# print(create_user.status_code)
+# print(create_user.json())
 
+
+# Получение пользователя
+get_user = requests.get("http://127.0.0.1:8000/api/v1/user/100")
+print(get_user.status_code)
+print(get_user.json())
