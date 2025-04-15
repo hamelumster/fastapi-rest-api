3daa5cbb68c6f5b6fa39109e157e769afc7301b0
Nicki tst <kanieleoutiz@gmail.com>
2025-04-16 00:03:07 +0300

    add get_adv request

--- client.py
+++ client.py
@@ -1,4 +1,6 @@
 import requests
+from pprint import pprint
+
 
 # Создание пользователя
 # create_user = requests.post(
@@ -14,13 +16,20 @@ import requests
 # print(get_user.status_code)
 # print(get_user.json())
 
+
 # Создание объявления
-create_adv = requests.post(
-    "http://127.0.0.1:8000/api/v1/advertisement/",
-    json={"title": "shoes",
-          "description": "very good shoes",
-          "price": 1000,
-          "author": "user_4"}
-)
-print(create_adv.status_code)
-print(create_adv.json())
+# create_adv = requests.post(
+#     "http://127.0.0.1:8000/api/v1/advertisement/",
+#     json={"title": "shoes",
+#           "description": "very good shoes",
+#           "price": 1000,
+#           "author": "user_4"}
+# )
+# print(create_adv.status_code)
+# print(create_adv.json())
+
+
+# Получение объявления
+get_adv = requests.get("http://127.0.0.1:8000/api/v1/advertisement/4")
+print(get_adv.status_code)
+pprint(get_adv.json())
