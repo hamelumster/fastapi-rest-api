a1586a32ea9d5a4df6518e616cb57186e84428c9
Nicki tst <kanieleoutiz@gmail.com>
2025-04-16 16:07:55 +0300

    fix PG_DSN for local and from container access

--- db/Config.py
+++ db/Config.py
@@ -2,7 +2,7 @@ import os
 
 POSTGRES_DB = os.getenv("POSTGRES_DB", "rest_fastapi")
 POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
-POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5431")
 POSTGRES_USER = os.getenv("POSTGRES_USER", "app")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "secret")
 
