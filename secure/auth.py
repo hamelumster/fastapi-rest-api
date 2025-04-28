5d86070f4492cfd03d9342439b25e8bb927c203b
Nicki tst <kanieleoutiz@gmail.com>
2025-04-28 03:18:18 +0300

    totally change get_current_user

--- secure/auth.py
+++ secure/auth.py
@@ -2,7 +2,7 @@ import bcrypt
 from fastapi import Header, HTTPException, Depends
 from sqlalchemy import select
 
-from db.dependency import SessionDependency
+from db.dependency import SessionDependency, TokenDependency
 from db.models import User, Token
 
 
@@ -18,22 +18,5 @@ def check_password(password: str, password_hashed: str) -> bool:
     return bcrypt.checkpw(password, password_hashed)
 
 
-async def get_current_user(
-        session: SessionDependency,
-        authorization: str | None = Header(None)
-) -> User | None:
-    if not authorization:
-        return None
-
-    try:
-        scheme, token = authorization.split()
-        if scheme.lower() != "bearer":
-            raise ValueError
-    except ValueError:
-        raise HTTPException(401, detail="Invalid auth scheme")
-
-    stmt = select(Token).where(Token.token == token)
-    token_obj = (await session.scalar(stmt)).scalar_one_or_none()
-    if not token_obj:
-        raise HTTPException(401, detail="Invalid token")
-    return token_obj.user
+async def get_current_user(token: Token = TokenDependency) -> User:
+    return token.user
