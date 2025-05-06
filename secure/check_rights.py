3a1e6ec63e991fda9a02c1a933919fe00c8092ff
Nicki tst <kanieleoutiz@gmail.com>
2025-05-06 23:12:51 +0300

    change dependency()

--- secure/check_rights.py
+++ secure/check_rights.py
@@ -1,4 +1,4 @@
-from typing import Callable
+from typing import Annotated
 
 from fastapi import Depends, HTTPException
 
@@ -6,8 +6,10 @@ from db.models import User
 from secure.auth import get_current_user
 
 
+GET_CURRENT_USER_DEPENDENCY = Annotated[User, Depends(get_current_user)]
+
 def require_role(*allowed_roles: str):
-    async def dependency(current_user: User = Depends(get_current_user)):
+    async def dependency(current_user: GET_CURRENT_USER_DEPENDENCY):
         names = [r.name for r in current_user.roles]
         if "admin" in names:
             return current_user
